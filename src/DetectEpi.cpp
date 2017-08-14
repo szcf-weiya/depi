f1e4c2ae0b4f4bc6ca3e2d5e5883f19fc5dcd0de
szcf-weiya <2215235182@qq.com>
2017-08-14 22:16:03 +0800

    update

--- src/DetectEpi.cpp
+++ src/DetectEpi.cpp
@@ -49,7 +49,7 @@ void readData(string FILE, vector<vector<double> > &mv, vector<string> &rowname,
     colname.push_back(tmp);
     (*ncol)++;
   }
-  cout << colname.size() << endl;
+  //cout << colname.size() << endl;
 
   while (getline(input, line)) {
     mv[*nrow].resize(*ncol);
@@ -63,7 +63,8 @@ void readData(string FILE, vector<vector<double> > &mv, vector<string> &rowname,
     {
       ss >> tmp;
       val = strtod(tmp.c_str(), NULL); // transfer str to double
-      mv[*nrow].push_back(val);
+      //mv[*nrow].push_back(val);
+      mv[*nrow][j] = val;
     }
     (*nrow)++;
   }
@@ -126,52 +127,31 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, boo
   vector<vector<double> > mv(1, vector<double>(1));
   size_t nrow, ncol;
   readData(file_X.c_str(), mv, G_rowname, G_colname, &nrow, &ncol);
-  gsl_matrix *G = gsl_matrix_alloc(nrow, ncol);
+  cout << ncol << endl;
+  gsl_matrix *G = gsl_matrix_calloc(nrow, ncol);
+  cout << mv.size() << " " << mv[0].size() << endl;
+  cout << nrow << " " << ncol << endl;
+  cout << G->size1 << " " << G->size2 << endl;
+  cout << mv[0][0] << endl;
+  cout << mv[0][10] << endl;
   for (size_t i = 0; i < nrow; i++)
     for (size_t j = 0; j < ncol; j++)
-      gsl_matrix_set(G, i, j, vector[i][j]);
+      gsl_matrix_set(G, i, j, mv[i][j]);
 
+  int p;
+  if (IS_EXIST_COV)
+    p = 4 + COV_COL;
+  else
+    p = 4;
+  gsl_matrix *X = gsl_matrix_alloc(nrow, p);
   if (IS_EXIST_COV)
   {
-    gsl_matrix *coveriate = gsl_matrix_alloc(MAX_ROW, COV_COL);
+    gsl_matrix *coveriate = gsl_matrix_alloc(nrow, COV_COL);
     // read data cov
     vector<string> cov_colname;
     vector<string> cov_rowname;
     readData(file_COV.c_str(), coveriate, nrow, COV_COL, cov_rowname, cov_colname);
-  }
-
-
-  // read data Y
-  ifstream input2(file_Y.c_str());
-  double val;
-  gsl_vector *Y = gsl_vector_alloc(MAX_ROW);
-  for (size_t i = 0; i < MAX_ROW; i++)
-  {
-    input2 >> val;
-    gsl_vector_set(Y, i, val);
-  }
-  input2.close();
-
-
-  // fit
-  gsl_vector *x0 = gsl_vector_alloc(MAX_ROW);
-  gsl_vector_set_all(x0, 1);
-  //  gsl_vector *x1_copy = gsl_vector_alloc(MAX_ROW);
-
-  int p;
-  int max_val, min_val;
-
-  double min_pair_geno;
-  gsl_vector *coveriate_vec;
-  coveriate_vec = gsl_vector_alloc(MAX_ROW);
-  if (!IS_EXIST_COV)
-    p = 4;
-  else
-    p = 4 + COV_COL;
-  gsl_matrix *X;
-  X = gsl_matrix_alloc(MAX_ROW, p);
-  if (IS_EXIST_COV)
-  {
+    gsl_vector *coveriate_vec = gsl_vector_alloc(nrow);
     for (int ip = 0; ip < COV_COL; ip++)
     {
       gsl_matrix_get_col(coveriate_vec, coveriate, ip);
@@ -180,6 +160,28 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, boo
     gsl_vector_free(coveriate_vec);
     gsl_matrix_free(coveriate);
   }
+
+  // read data Y
+  ifstream input2(file_Y.c_str());
+  double val;
+  gsl_vector *Y = gsl_vector_alloc(nrow);
+  for (size_t i = 0; i < nrow; i++)
+  {
+    input2 >> val;
+    gsl_vector_set(Y, i, val);
+  }
+  input2.close();
+
+
+  // fit
+  gsl_vector *x0 = gsl_vector_alloc(nrow);
+  gsl_vector_set_all(x0, 1);
+  //  gsl_vector *x1_copy = gsl_vector_alloc(MAX_ROW);
+
+
+  int max_val, min_val;
+
+  double min_pair_geno;
 
   // cout << "  Number of processors available = " << omp_get_num_procs ( ) << "\n";
   // cout << "  Number of threads =              " << omp_get_max_threads ( ) << "\n";
@@ -192,8 +194,9 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, boo
   //# pragma omp parallel
   //# pragma omp for schedule(dynamic) // relatively slow
   gsl_matrix_set_col(X, 0, x0);
-  gsl_vector *x1 = gsl_vector_alloc(MAX_ROW);
-  for (int i = 0; i < MAX_COL; i++)
+  gsl_vector *x1 = gsl_vector_alloc(nrow);
+  cout << "hello "<< endl;
+  for (int i = 0; i < ncol/100; i++)
   {
 # pragma omp sections
 {
@@ -208,15 +211,15 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, boo
       continue;
     gsl_matrix_set_col(X, 1, x1);
     # pragma omp parallel for schedule(dynamic) // faster!!!
-    for (int j = i+1; j < MAX_COL; j++)
+    for (int j = i+1; j < ncol; j++)
     {
-      gsl_vector *x2 = gsl_vector_alloc(MAX_ROW);
+      gsl_vector *x2 = gsl_vector_alloc(nrow);
 
       size_t flag, df;
 
       double stderr, t;
       double pvalue[3];
-      df = MAX_ROW - p;
+      df = nrow - p;
 
       gsl_matrix_get_col(x2, G, j);
       // if same
@@ -229,10 +232,10 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, boo
       }
       gsl_matrix_set_col(X, 2, x2);
 
-      double res_corr = (float)gsl_stats_correlation(x1->data, 1, x2->data, 1, MAX_ROW);
+      double res_corr = (float)gsl_stats_correlation(x1->data, 1, x2->data, 1, nrow);
       if (fabs(res_corr) >= MAX_PAIR_CORR)
         continue;
-      gsl_vector *x3 = gsl_vector_alloc(MAX_ROW);
+      gsl_vector *x3 = gsl_vector_alloc(nrow);
       gsl_matrix_get_col(x3, G, i);
       gsl_vector_mul(x3, x2);
       gsl_vector_free(x2);
@@ -249,7 +252,7 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, boo
 
       gsl_vector_free(x3);
 
-      gsl_multifit_linear_workspace *work = gsl_multifit_linear_alloc(MAX_ROW, p);
+      gsl_multifit_linear_workspace *work = gsl_multifit_linear_alloc(nrow, p);
       gsl_matrix *cov;
       gsl_vector *coef;
       double chisq;
