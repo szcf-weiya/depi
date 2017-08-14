f5c6126ac4f9ba356738c8fce470fbc70c9635d3
szcf-weiya <2215235182@qq.com>
2017-08-14 14:12:39 +0800

    update

--- src/DetectEpi.cpp
+++ src/DetectEpi.cpp
@@ -31,6 +31,48 @@ using namespace std;
 #define coef(i) (gsl_vector_get(coef, (i)))
 #define COV(i, j) (gsl_matrix_get(cov, (i), (j)))
 
+// readData when unknown nrow and ncol
+void readData(string FILE, vector<vector<double> > &mv, vector<string> &rowname, vector<string> &colname, size_t *nrow, size_t *ncol)
+{
+  ifstream input(FILE.c_str());
+  string line;
+  string tmp, tmpp;
+  double val;
+  size_t nrow = 0, ncol = 0;
+  // the first line
+  getline(input, line);
+  stringstream ss(line);
+  while(!ss.eof())
+  {
+    ss >> tmp;
+    if(ss.eof())
+      break;
+    colname.push_back(tmp);
+    ncol++;
+  }
+
+  while (getline(input, line)) {
+    mv[nrow].resize(ncol);
+    if(mv.size() == nrow+1)
+      mv.resize(mv.size()*2);
+    ss.clear();
+    stringstream ss(line);
+    ss >> tmp; // rowname
+    rowname.push_back(tmp);
+    for (size_t j = 0; j < ncol; j++)
+    {
+      ss >> tmp;
+      val = strtod(tmp.c_str(), NULL); // transfer str to double
+      mv[nrow].push_back(val);
+    }
+    nrow++;
+  }
+  input.close();
+  mv.resize(nrow);
+  cout << "ncol = " << ncol << endl
+       << "nrow = " << nrow << endl;
+}
+
 void readData(string FILE, gsl_matrix *m, int row, int col, vector<string> &rowname, vector<string> &colname)
 {
   ifstream input(FILE.c_str());
@@ -76,19 +118,25 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, int
   string file_X = Rcpp::as<string>(r_inputfile_X);
   string file_Y = Rcpp::as<string>(r_inputfile_Y);
   string file_COV = Rcpp::as<string>(r_inputfile_COV);
-  
+
   // read data X
   gsl_matrix *G = gsl_matrix_alloc(MAX_ROW, MAX_COL);
   vector<string> G_colname;
   vector<string> G_rowname;
-  readData(file_X.c_str(), G, MAX_ROW, MAX_COL, G_rowname, G_colname);
-  
+  //readData(file_X.c_str(), G, MAX_ROW, MAX_COL, G_rowname, G_colname);
+  vector<vector<double> > mv(1, vector<double>(1));
+  size_t nrow, ncol;
+  readData(file_X.c_str(), mv, G_rowname, G_colname, &nrow, &ncol);
+  for (size_t i = 0; i < nrow; i++)
+    for (size_t j = 0; j < ncol; j++)
+      gsl_matrix_set(G, i, j, vector[i][j]);
+
   // read data cov
   gsl_matrix *coveriate = gsl_matrix_alloc(MAX_ROW, COV_COL);
   vector<string> cov_colname;
   vector<string> cov_rowname;
   readData(file_COV.c_str(), coveriate, MAX_ROW, COV_COL, cov_rowname, cov_colname);
-  
+
   // read data Y
   ifstream input2(file_Y.c_str());
   double val;
@@ -99,16 +147,16 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, int
     gsl_vector_set(Y, i, val);
   }
   input2.close();
-  
-  
+
+
   // fit
   gsl_vector *x0 = gsl_vector_alloc(MAX_ROW);
   gsl_vector_set_all(x0, 1);
   //  gsl_vector *x1_copy = gsl_vector_alloc(MAX_ROW);
-  
+
   int p;
   int max_val, min_val;
-  
+
   double min_pair_geno;
   gsl_vector *coveriate_vec;
   coveriate_vec = gsl_vector_alloc(MAX_ROW);
@@ -128,14 +176,14 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, int
   }
   gsl_vector_free(coveriate_vec);
   gsl_matrix_free(coveriate);
-  
+
   // cout << "  Number of processors available = " << omp_get_num_procs ( ) << "\n";
   // cout << "  Number of threads =              " << omp_get_max_threads ( ) << "\n";
   //RcppGSL::vector<double> r1, r2, r1_p, r2_p, r1r2_p;
   vector<string> r1, r2;
   vector<double> r1_p, r2_p, r1r2_p;
 
-  
+
 //  fprintf(output, "r1\tr2\tr1.p.value\tr2.p.value\tr1*r2.p.value\n");
   //# pragma omp parallel
   //# pragma omp for schedule(dynamic) // relatively slow
@@ -159,13 +207,13 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, int
     for (int j = i+1; j < MAX_COL; j++)
     {
       gsl_vector *x2 = gsl_vector_alloc(MAX_ROW);
-      
+
       size_t flag, df;
-      
+
       double stderr, t;
       double pvalue[3];
       df = MAX_ROW - p;
-      
+
       gsl_matrix_get_col(x2, G, j);
       // if same
       max_val = gsl_vector_max(x2);
@@ -176,7 +224,7 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, int
         continue;
       }
       gsl_matrix_set_col(X, 2, x2);
-      
+
       double res_corr = (float)gsl_stats_correlation(x1->data, 1, x2->data, 1, MAX_ROW);
       if (fabs(res_corr) >= MAX_PAIR_CORR)
         continue;
@@ -184,7 +232,7 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, int
       gsl_matrix_get_col(x3, G, i);
       gsl_vector_mul(x3, x2);
       gsl_vector_free(x2);
-      
+
       max_val = gsl_vector_max(x3);
       //	  min_val = gsl_vector_min(x3);
       if (max_val == 0)
@@ -192,22 +240,22 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, int
         gsl_vector_free(x3);
         continue;
       }
-      
+
       gsl_matrix_set_col(X, 3, x3);
-      
+
       gsl_vector_free(x3);
-      
+
       gsl_multifit_linear_workspace *work = gsl_multifit_linear_alloc(MAX_ROW, p);
       gsl_matrix *cov;
       gsl_vector *coef;
       double chisq;
       cov = gsl_matrix_alloc(p, p);
       coef = gsl_vector_alloc(p);
-      
+
       gsl_multifit_linear(X, Y, coef, cov, &chisq, work);
       gsl_multifit_linear_free(work);
       flag = 1;
-      
+
       for (int k = 1; k < p; k++){
         if (coef(k) > MAX_EPS)
           flag = 0;
@@ -232,13 +280,13 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, int
       pvalue[0] = 2*(t < 0 ? (1 - gsl_cdf_tdist_P(-t, df)) : (1 - gsl_cdf_tdist_P(t, df)));
       gsl_matrix_free(cov);
       gsl_vector_free(coef);
-      
+
       r1.push_back(G_colname[i]);
       r2.push_back(G_colname[j]);
       r1_p.push_back(pvalue[0]);
       r2_p.push_back(pvalue[1]);
       r1r2_p.push_back(pvalue[2]);
-      
+
     }
   }
   Rcpp::DataFrame output = Rcpp::DataFrame::create(Rcpp::Named("r1") = r1,
@@ -246,12 +294,12 @@ Rcpp::List DetectEpi(SEXP inputfile_X, SEXP inputfile_Y, SEXP inputfile_COV, int
                                                    Rcpp::Named("r1.p.value") = r1_p,
                                                    Rcpp::Named("r2.p.value") = r2_p,
                                                    Rcpp::Named("r1*r2.p.value") = r1r2_p);
-  
+
   gsl_vector_free(x0);
   gsl_vector_free(x1);
   gsl_matrix_free(X);
   gsl_vector_free(Y);
   gsl_matrix_free(G);
-  
+
   return output;
 }
