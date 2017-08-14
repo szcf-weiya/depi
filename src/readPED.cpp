296937a492feddd6429459ca23082e93b7a75723
szcf-weiya <2215235182@qq.com>
2017-08-15 01:33:15 +0800

    v0.2.0

--- src/readPED.cpp
+++ src/readPED.cpp
@@ -16,44 +16,13 @@
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_eigen.h>
 
+#include "safeGetline.h"
+
 using namespace std;
 
 #define ERR 1e-10
 
-// from https://stackoverflow.com/questions/6089231/getting-std-ifstream-to-handle-lf-cr-and-crlf/6089413#6089413
 
-std::istream& safeGetline(std::istream& is, std::string& t)
-{
-    t.clear();
-
-    // The characters in the stream are read one-by-one using a std::streambuf.
-    // That is faster than reading them one-by-one using the std::istream.
-    // Code that uses streambuf this way must be guarded by a sentry object.
-    // The sentry object performs various tasks,
-    // such as thread synchronization and updating the stream state.
-
-    std::istream::sentry se(is, true);
-    std::streambuf* sb = is.rdbuf();
-
-    for(;;) {
-        int c = sb->sbumpc();
-        switch (c) {
-        case '\n':
-            return is;
-        case '\r':
-            if(sb->sgetc() == '\n')
-                sb->sbumpc();
-            return is;
-        case EOF:
-            // Also handle the case when the last line has no line ending
-            if(t.empty())
-                is.setstate(std::ios::eofbit);
-            return is;
-        default:
-            t += (char)c;
-        }
-    }
-}
 
 void readPED(string FILE, vector<vector<double> > &mv, size_t &n0, size_t &nh, size_t &n1, size_t &nNA)
 {
